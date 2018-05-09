cb9314e4d0aafd710080fe3da9aa5d69dfd8a48b
Anna <anna.zurczak@student.put.poznan.pl>
2018-05-09 20:10:59 +0200

    frangi

--- bloodVesselsDetection.py
+++ bloodVesselsDetection.py
@@ -4,7 +4,7 @@ import numpy as np
 from math import *
 import cv2
 from skimage.draw import circle
-from skimage.filters import frangi
+from skimage import filters
 from sklearn.metrics import confusion_matrix
 
 def compareMatrixes(expert, algorithm):
@@ -26,7 +26,7 @@ def compareMatrixes(expert, algorithm):
     print("Sensitivity = ", sensitivity, "Specificity = ", specificity, "Accuracy = ", accuracy)
 
 def main():
-    fileName = "08_h.jpg"
+    fileName = "03_h.jpg"
     catalogName = "healthy"
     image = io.imread(catalogName + "/" + fileName)
     plt.subplot(221)
@@ -35,17 +35,14 @@ def main():
     outputImage = image[:, :, 1] #green channel
     plt.subplot(222)
     io.imshow(outputImage)
+    image = filters.frangi(outputImage)
+#    outputImage = cv2.imread(catalogName + "/" + fileName)
+#    outputImage = cv2.cvtColor(outputImage, cv2.COLOR_BGR2GRAY)
+#    outputImage = cv2.adaptiveThreshold(outputImage, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 33, 2)
 
-    outputImage = cv2.imread(catalogName + "/" + fileName)
-    outputImage = cv2.cvtColor(outputImage, cv2.COLOR_BGR2GRAY)
-    outputImage = cv2.adaptiveThreshold(outputImage, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 33, 2)
-
-    myMask = np.zeros((3, 3), np.uint8)
-    myMask[1, :] = 1
-    myMask[:, 1] = 1
 
     #outputImage = cv2.medianBlur(outputImage, 9)
-    outputImage = morphology.dilation(outputImage)
+    ''' outputImage = morphology.dilation(outputImage)
     outputImage = morphology.dilation(outputImage)
     outputImage = morphology.erosion(outputImage)
     outputImage = morphology.erosion(outputImage)
@@ -53,16 +50,34 @@ def main():
     outputImage = cv2.medianBlur(outputImage, 11)
     outputImage = cv2.medianBlur(outputImage, 11)
     #outputImage = cv2.medianBlur(outputImage, 5)
-    outputImage = cv2.bitwise_not(outputImage)
+    outputImage = cv2.bitwise_not(outputImage)'''
+
+    image2 = np.zeros_like(image)
+    temp = np.zeros_like(image[0])
+    i = 0
+    for val in image:
+        j = 0
+        for x in val:
+            if x > 0.0000001:
+                temp[j] = 255
+            else:
+                temp[j] = 0
+            j += 1
+        image2[i] = temp
+        i += 1
+        temp = np.zeros_like(image[0])
+
+    outputImage=image2
     plt.subplot(223)
-    io.imshow(outputImage)
+    io.imshow(outputImage,cmap='gray')
 
 
-    expertImage = cv2.imread("healthy_manualsegm/08_h.tif")
+
+    expertImage = cv2.imread("healthy_manualsegm/03_h.tif")
     expertImage = cv2.cvtColor(expertImage, cv2.COLOR_BGR2GRAY)
     plt.subplot(224)
     io.imshow(expertImage)
-
+    plt.show()
     compareMatrixes(expertImage, outputImage)
 
     '''
